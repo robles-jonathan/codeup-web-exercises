4134053b45e4ef8655f38235d5934a76ae23b80d
Jonathan Robles <jonathan.robles287@gmail.com>
2021-10-08 10:49:08 -0500

    completed the js with html lecture

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let jeff;
+jeff = "Jeff";
+console.log(jeff);
+
+console.log("Hello I am in my own file now!");
+
+var returnOfAlert = alert("TFIG! Thank Goodness I'm Fred");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("got any plans for this three day weekend?");
+console.log(returnOfConfirm);
+
+var favoriteNuMetalBand = prompt("What's your favorite nu-metal band?");
+console.log("HAHA, you like " + favoriteNuMetalBand + "!");
+
+var leastFavoriteSoda = prompt("Whats your least favorite soda?");
+var favoriteSoda = prompt("What's your favorite soda?");
+
+alert("Hello fellow teens, my name is " + leastFavoriteSoda + " " + favoriteSoda + " and I choose hugs not drugs!");
