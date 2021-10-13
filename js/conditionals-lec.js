27f4ecd525f3ea3d7a9e1487f63c50d65d8e5258
Jonathan Robles <jonathan.robles287@gmail.com>
2021-10-13 10:39:18 -0500

    Finished Conditionals Lecture

--- js/conditionals-lec.js
+++ js/conditionals-lec.js
@@ -1,4 +1,4 @@
-// Conditionals: Code that runs when we meeet certain requirements.
+// Conditionals: Code that runs when we meet certain requirements.
 
 // IF STATEMENTS - Code that runs when a condition is met.
 
@@ -25,15 +25,112 @@ if(five === 5){
 /** TO-DO: I want to build a function that returns a string when I pass a number as
  * an argument. */
 
-function isItANumber(parameter) {
-    var str = "Oi, that aint a number."
-    if(typeof parameter === "number"){
-        str = "Hey, That's a number.";
+// function isItANumber(parameter) {
+//     var str = "Oi, that aint a number."
+//     if(typeof parameter === "number"){
+//         str = "Hey, That's a number.";
+//     }
+//     return str;
+// }
+//
+// console.log(isItANumber(73));
+// console.log(isItANumber("Doug"));
+
+
+// IF/ELSE IF// ELSE FUNCTIONS
+
+
+function isThisThirtyFive(parameter) {
+    if (parameter > 35){
+        return "No. " + parameter + " is greater than 35.";
+    }else if ( parameter < 35){
+        return "No. " + parameter + " is less than 35.";
     }
-    return str;
+    else{
+        return "Yes!! That sure is 35!!";
+    }
 }
 
+console.log(isThisThirtyFive(34));
+console.log(isThisThirtyFive(36));
+console.log(isThisThirtyFive(-35));
+
+
+// TERNARY EXPRESSION: A expression to set a value based on a boolean evaluation.
+
+// var message = (booleanValue) ? "Expression was true." : "Expression was false.";
+
+/** Consider the following statement: "Are we out of milk? If we are: I'll head to the store, but if we do have milk: I want to make a bowl of cereal." */
+
+// How would we write this as a ternary expression?
+
+var doWeHaveMilk = false;
+
+var action = (doWeHaveMilk) ? "Time to eat some corn pops." : "Time to go to the store.";
+
+console.log(action);
+
+
+
+function isItANumber(parameter) {
+    return (typeof parameter === "number") ? "Hey, That's a number." : "Hey, you're not a number!";
+    }
 console.log(isItANumber(73));
 console.log(isItANumber("Doug"));
 
+// SWITCH STATEMENT: A statement with multiple conditions that will trigger based on the value of a variable.
+
+var color = prompt("what is your favorite color?").toLowerCase();
+
+switch (color) {
+    case "blue": //will return same as the next
+    case "green":
+        alert("What a coincidence, that's my favorite color!");
+        break;
+    case "orange":
+        alert("That's my brother's favorite color!");
+        break;
+    default: // default should always be last
+        alert(color + " is a weird color, my dude.");
+        break;
+}
+
+
+/** Consider the following sentence: "I dont have a cow, goat, or camel. I guess I'll have to go to the store for milk." */
+
+// How would we write this as a switch statement?
+
+var animal = "Blue Tongued Skink";
+switch (animal) {
+    case "cow":
+        console.log("Milking Bessie.");
+        break;
+    case "goat":
+        console.log("Milking Gertrude.");
+        break;
+    case "camel":
+        console.log("Milking Camille.");
+        break;
+    default:
+        console.log("Headed to Kroger for milk, want anything?");
+        break;
+}
+
+/** TO-DO: Lets make a switch statement with a prompt for the user to input their facorite sandwich meat. If they live 'olive loaf'
+ * we should return an alert to congratulate them on their bold choice, and if they chose 'brisket' we should return a differnet alert:
+ * this one for congratulating them on the 'right' answer. All other responses should trigger an alert stating apathy.
+ *  */
+
+var favLunchMeat = prompt ("What is your favorite Lunch Meat?").toLowerCase();
+
+switch (favLunchMeat) {
+    case "olive loaf":
+        alert("You have a bold palette; good for you!");
+        break;
+    case "brisket":
+        alert("Congratulations, you chose the right answer.");
+        break;
+    default:
+        alert("That's fine. Nothing wrong with that.");
+}
 
---
+++ js/morning-warmups.js
@@ -0,0 +1,18 @@
+"use strict";
+(function(){
+    // Warm up for 10-13-21
+// Write a function that returns the number 7.
+    function returnSeven(){
+        return 7;
+    }
+    console.log(returnSeven());
+
+// Write an if statement with a condition set to the boolean false.
+// Does it run?
+
+    if (true){
+        console.log("Am I seen?");
+    }
+
+
+})();
