132531822bfad40ccfda4962596ed8ee0662e900
Jonathan Robles <jonathan.robles287@gmail.com>
2021-10-16 22:21:57 -0500

    Finished problem 2 from Edabit

--- js/js-extra-work.js
+++ js/js-extra-work.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+// Less Than 100
+
 //Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
 /*
 EXAMPLES
@@ -12,9 +15,30 @@ lessThan100(83, 34) ➞ false
 lessThan100(3, 77) ➞ true
 */
 function lessThan100(x,y){
-    return (x+y<100)
+    return (x+y<100);
 }
 
 console.log(lessThan100(22, 15));
 console.log(lessThan100(83, 34));
 console.log(lessThan100(3, 77));
+
+// Area of a Triangle
+/*
+Write a function that takes the base and height of a triangle and return its area.
+
+    Examples
+triArea(3, 2) ➞ 3
+
+triArea(7, 4) ➞ 14
+
+triArea(10, 10) ➞ 50
+
+ */
+
+function triArea(base, height) {
+    return ((base * height)/2);
+}
+
+console.log(triArea(3, 2));
+console.log(triArea(7, 4));
+console.log(triArea(10, 10));
