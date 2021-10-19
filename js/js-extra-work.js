3935c2a1e663a6f76f1b2045eba8fda3ed146649
Jonathan Robles <jonathan.robles287@gmail.com>
2021-10-19 09:31:36 -0500

    Morning Warmups

--- js/js-extra-work.js
+++ js/js-extra-work.js
@@ -43,8 +43,43 @@
     console.log(triArea(7, 4));
     console.log(triArea(10, 10));
 
-    let txt = "I can eat bananas all day";
-    let x = txt.slice(10, 17);
-    console.log(x);
+
+    //Bitwise Operations
+    /*
+    A decimal number can be represented as a sequence of bits. To illustrate:
+
+        6 = 00000110
+    23 = 00010111
+    From the bitwise representation of numbers, we can calculate the bitwise AND, bitwise OR and bitwise XOR. Using the example above:
+
+        bitwiseAND(6, 23) ➞ 00000110
+
+    bitwiseOR(6, 23) ➞ 00010111
+
+    bitwiseXOR(6, 23) ➞ 00010001
+    Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+        Examples
+    bitwiseAND(7, 12) ➞ 4
+
+    bitwiseOR(7, 12) ➞ 15
+
+    bitwiseXOR(7, 12) ➞ 11
+    Notes
+    JavaScript has a useful function: toString(2), where you can see the binary representation of a decimal number.
+
+     */
+
+    function bitwiseAND(n1,n2){
+        return n1 & n2;
+    }
+
+    function bitwiseOR(n1, n2) {
+        return n1 | n2;
+    }
+
+    function bitwiseXOR(n1, n2) {
+        return n1 ^ n2;
+    }
 
 })();
--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -44,4 +44,18 @@
     console.log("Input is 4 + '4': \n" + typeCheck(4 + '4'));
 
 
+    console.log("Morning Warmup \"FizzBuzz\"");
+    for(var i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz")
+        }else if(i % 5 === 0){
+            console.log("Buzz")
+        }else {
+            console.log(i);
+        }
+    }
+
+
 })();
