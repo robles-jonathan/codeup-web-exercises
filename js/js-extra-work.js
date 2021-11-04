d5d38baf6252aa0087730ad0425a1cd9a6d3d380
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-04 16:24:18 -0500

    Added footer to flexbox-nav, added some js extra work.

--- js/js-extra-work.js
+++ js/js-extra-work.js
@@ -1255,4 +1255,20 @@
     }
     console.log(sumOfNumbersUnderInput(10))
 
+
+    function Rectangle(a, b) {
+        return {
+            length: a,
+            width: b,
+            perimeter: 2*(a+b),
+            area: a*b,
+        }
+    }
+    const rec = new Rectangle(4,5);
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+
+
 })();
