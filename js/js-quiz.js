9f758f615fab3c0fe1462ccc4015c158db7988d9
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-15 09:56:11 -0600

    finished js quiz

---
+++ js/js-quiz.js
@@ -0,0 +1,54 @@
+"use strict";
+// var isColdOutside = true;
+
+// var clothingToWear;
+
+// if (isColdOutside) {
+//     clothingToWear = 'sweater and a jacket';
+//     console.log(clothingToWear)
+// } else {
+//     clothingToWear = 'shorts and a t-shirt';
+//     console.log(clothingToWear)
+// }
+
+// console.log(isColdOutside)
+// var clothingToWear = isColdOutside ? 'sweater and a jacket' : 'shorts and a t-shirt';
+// console.log(clothingToWear);
+//
+// console.log(10 % 3);
+// console.log(0)
+// console.log("")
+// console.log(!!true)
+//
+// var i = "true"
+// if (i = true){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// var i = 0;
+// while(i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+// array.join("");
+// console.log(array.join(" "));
+
+
+var band = new Object();
+
+
+//
+// var band = ();
+//
+//
+//
+// var band = {};
+//
+//
+//
+// Both var band = new Object(); and var band = {};
+
