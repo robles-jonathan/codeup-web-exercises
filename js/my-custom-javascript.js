73711e13cf0cb653cc96b43d9740fd901fcccbab
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-19 17:02:20 -0600

    Finished jquery mouse event exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 
     // Id Selectors
-
+    var mainTitle = $('#main-title').html('Changed the MAIN TITLE!!')
     // var idContents = $('#para1').html();
     // alert('The content of ID para1 is : ' + idContents);
     //
@@ -34,47 +34,30 @@ $(document).ready(function() {
     //Multiple Selectors
 
     // $('h1, p,li').css('background-color', 'green');
-})
 
-//Exercises
-// Continue working in jquery_exercises.html for these exercises. Make sure you are committing your changes to GitHub.
+
+    //MOUSE EVENTS
+    mainTitle.click(function(){
+        $(this).css('background','cadetblue');
+    });
+
+    var paragraphs = $('p');
+    paragraphs.dblclick(function(){
+        $(this).css('font-size','18px');
+    });
+
+    var li = $('li');
+    li.hover(function (){
+        $(this).css('color', 'red');
+    }, function (){
+        $(this).css('color', 'black');
+    });
+});
+
+//Remove your custom jQuery code from previous exercises.
 //
-// Id Selectors
+// Add jQuery code that will change the background color of an h1 element when clicked.
 //
-// Create content in your HTML file using at least the following elements: h1, p, ul, li, div.
+// Make all paragraphs have a font size of 18px when they are double clicked.
 //
-// Add several attributes to your elements; you will need both id and class attributes.
-//
-// Use jQuery to select an element by the id. Alert the contents of the element.
-//
-// Update the jQuery code to select and alert a different id.
-//
-// Use the same id on 2 elements. How does this change the jQuery selection?
-//
-// Remove the duplicate id. Each id should be unique on that page.
-//
-// Class Selectors
-//
-// Remove your custom jQuery code from previous exercises.
-//
-// Update your code so that at least 3 different elements have the same class named codeup.
-//
-// Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
-//
-// Remove the class from one of the elements. Refresh and test that the border has been removed.
-//
-// Give another element an id of codeup. Does this element get a border now?
-//
-// Element Selectors
-//
-// Remove your custom jQuery code from previous exercises.
-//
-// Using jQuery, set the font-size of all li elements to 20px.
-//
-// Craft selectors that highlight all the h1, p, and li elements.
-//
-// Create a jQuery statement to alert the contents of your h1 element(s).
-//
-// Multiple Selectors
-//
-// Combine your selectors that highlight all the h1, p, and li elements.
+// Set all li text color to red when the mouse is hovering; reset to black when it is not.
