56ad367acadbcf248b1798290e997375e3a89ef9
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-19 12:13:03 -0600

    Finished ID SELECTORS section

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,4 +1,70 @@
 "use strict";
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    // alert( 'The DOM has finished loading!' );
+
+
+    // Id Selectors
+
+    var idContents = $('#para1').html();
+    alert('The content of ID para1 is : ' + idContents);
+
+    var liFourContents = $('#liFour').html();
+    alert('The content of ID liFour is : ' + liFourContents);
+
+    //Use the same id on 2 elements. How does this change the jQuery selection?
+    //Will only select the first ID not the second.
+
+    var idOfLi = $('#li').html();
+    alert('The content of ID li is : ' + idOfLi);
+
+    //Class Selectors
+    // $('.codeup').css('border','1px solid red');
+    //Give another element an id of codeup. Does this element get a border now?
+    // No that element does that get a border.
+
+    //Element Selectors
+
 })
+
+//Exercises
+// Continue working in jquery_exercises.html for these exercises. Make sure you are committing your changes to GitHub.
+//
+// Id Selectors
+//
+// Create content in your HTML file using at least the following elements: h1, p, ul, li, div.
+//
+// Add several attributes to your elements; you will need both id and class attributes.
+//
+// Use jQuery to select an element by the id. Alert the contents of the element.
+//
+// Update the jQuery code to select and alert a different id.
+//
+// Use the same id on 2 elements. How does this change the jQuery selection?
+//
+// Remove the duplicate id. Each id should be unique on that page.
+//
+// Class Selectors
+//
+// Remove your custom jQuery code from previous exercises.
+//
+// Update your code so that at least 3 different elements have the same class named codeup.
+//
+// Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+//
+// Remove the class from one of the elements. Refresh and test that the border has been removed.
+//
+// Give another element an id of codeup. Does this element get a border now?
+//
+// Element Selectors
+//
+// Remove your custom jQuery code from previous exercises.
+//
+// Using jQuery, set the font-size of all li elements to 20px.
+//
+// Craft selectors that highlight all the h1, p, and li elements.
+//
+// Create a jQuery statement to alert the contents of your h1 element(s).
+//
+// Multiple Selectors
+//
+// Combine your selectors that highlight all the h1, p, and li elements.
