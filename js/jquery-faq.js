24fcf5ae2179196aeab904af231d1304a703dfab
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-29 17:01:25 -0600

    finished jquery second exercise

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -14,4 +14,21 @@ $(document).ready(function() {
     $('dt').click(function(){
         $(this).addClass('highlight');
     })
+
+    // Use button to change background to last LI to all ULs
+    $('#btn-1').click(function(){
+        $('ul').each(function (index, element) {
+            $(element).children().last().css('background', 'yellow');
+        });
+    });
+
+    $('h3').click(function(){
+        $(this).next().css('font-weight', 'bold');
+    })
+
+    $('li').click(function(){
+        $(this).parent().children().first().css('color', 'blue');
+    })
+
+
 })
