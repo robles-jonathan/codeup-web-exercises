b5f8f21cb4366f7c973075a0ceb6d8c6bdfe0e15
Jonathan Robles <jonathan.robles287@gmail.com>
2021-11-30 11:37:58 -0600

    finsihed functionality and styling of Picture Swapper

--- js.jquery-picture-swapper.js
+++ js.jquery-picture-swapper.js
@@ -1,4 +1,31 @@
 "use strict";
-$(document).ready(function(){
+$(document).ready(function () {
+    //Click Function used to toggle first and second frame
+    $('#btn1').click(function () {
+        var oldClass = $(this).prev().attr('class').split(' ')[1];
+        var newClass = $(this).parent().next().children().first().attr('class').split(' ')[1];
+        //Toggles first frame class
+        $(this).prev().attr('class', 'frame ' + newClass)
+        //Toggles second frame class
+        $(this).parent().next().children().first().attr('class', 'frame ' + oldClass);
+    })
+
+    //Click Function used to toggle middle frame with random frame
+    $('#btn2').click(function () {
+        var choice = Math.floor(Math.random() * 2);
+        var frameChoice = (choice) ? $(this).parent().prev() : $(this).parent().next();
+        var oldClass = $(this).prev().attr('class').split(' ')[1];
+        var newClass = frameChoice.children().first().attr('class').split(' ')[1];
+        $(this).prev().attr('class', 'frame ' + newClass);
+        frameChoice.children().first().attr('class', 'frame ' + oldClass);
+    })
+
+    //Click Function used to toggle second and last frame
+    $('#btn3').click(function () {
+        var oldClass = $(this).prev().attr('class').split(' ')[1];
+        var newClass = $(this).parent().prev().children().first().attr('class').split(' ')[1];
+        $(this).prev().attr('class', 'frame ' + newClass);
+        $(this).parent().prev().children().first().attr('class', 'frame ' + oldClass);
+    })
 
 })
