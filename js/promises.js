0d3a34d0948ef5676ef31bd655c4add162a0e898
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-10 09:04:42 -0600

    finished walk thru of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,31 +1,44 @@
 "use strict";
 
-$(document).ready(()=> {
+$(document).ready(() => {
+
+    const getLastCommit = (user) => {
+        let eventsURL = `https://api.github.com/users/${user}/events`;
+        fetch(eventsURL, {headers: {'Authorization': `token ${GITHUB_KEY}`}})
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                for (let event of data) {
+                    if (event.type === "PushEvent") {
+                        console.log(new Date(event.created_at));
+                        break;
+                    }
+                }
+            });
+    }
+
+    // fetch('https://api.github.com/users')
+    //     .then(resp => resp.json()).then(data => console.log(data))
+    //     .catch(error => console.error(error));
+    //
+    //
+    // let repo = 'codeup-web-exercises'
+    // let url = `https://api.github.com/repos/${user}/${repo}/commits`;
+    // fetch(url, {headers: {'Authorization': 'GITHUB_KEY'}}).then(resp => resp.json()).then(data => console.log(data));
+    // fetch(url, {headers: {'Authorization': 'GITHUB_KEY'}}).then(resp => resp.json()).then(data => console.log(data[0].commit.author.date));
 
     getLastCommit('robles-jonathan');
 
-    function getLastCommit(user){
-        let eventsURL = `https://api.github.com/users/${user}/events`;
-        fetch(eventsURL,{headers: {'Authorization': 'GITHUB_KEY'}}).then(resp => resp.json()).then(data => console.log(data[0].created_at));
-
-        // fetch('https://api.github.com/users')
-        //     .then(resp => resp.json()).then(data => console.log(data))
-        //     .catch(error => console.error(error));
-        //
-        //
-        // let repo = 'codeup-web-exercises'
-        // let url = `https://api.github.com/repos/${user}/${repo}/commits`;
-        // fetch(url, {headers: {'Authorization': 'GITHUB_KEY'}}).then(resp => resp.json()).then(data => console.log(data));
-        // fetch(url, {headers: {'Authorization': 'GITHUB_KEY'}}).then(resp => resp.json()).then(data => console.log(data[0].commit.author.date));
-    }
-    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
-    function wait(milli){
-        return new Promise((resolve, reject) => {
-            setTimeout(()=>{resolve()}, milli);
+    const wait = (milli) => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve()
+            }, milli);
         });
     }
 
+    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 //     function makeRequest(milli) {
 //         return new Promise((resolve, reject) => {
 //             setTimeout(() => {
@@ -44,6 +57,6 @@ $(document).ready(()=> {
 // // if resolved, will log "Promise resolved!" and "Here is your data: ..."
 //     request.catch(message => console.log('Promise rejected!', message));
 // // if rejected, will log "Promise rejected!" and "Network Connection Error!"
-})
+})();
 
 
